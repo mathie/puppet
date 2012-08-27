89113a3820f6598654613986da1061a9bbc9472c
Graeme Mathieson <mathie@woss.name>
2012-08-27 21:05:25 +0100

    Make sure the tun/tap dev node is around.
    Seems to be missing on LXC containers.

--- modules/openvpn/manifests/install.pp
+++ modules/openvpn/manifests/install.pp
@@ -3,4 +3,19 @@ class openvpn::install {
     'openvpn':
       ensure => present,
   }
+
+  file {
+    '/dev/net':
+      ensure => directory,
+      owner  => root,
+      group  => root,
+      mode   => '0644';
+  }
+
+  exec {
+    'create /dev/net/tun':
+      command => '/bin/mknod -m 0666 /dev/net/tun c 10 200',
+      creates => '/dev/net/tun',
+      require => File['/dev/net'];
+  }
 }
