1b5edd6ece23e7aa75f14c540945e0b3644c5329
Graeme Mathieson <mathie@woss.name>
2012-08-27 15:50:12 +0100

    Manage ssh known hosts for all hosts in the local network.

--- modules/ssh/manifests/client/config.pp
+++ modules/ssh/manifests/client/config.pp
@@ -1,10 +1,10 @@
 class ssh::client::config {
-  file {
-    '/etc/ssh/ssh_known_hosts':
-      ensure => present,
-      source => 'puppet:///modules/ssh/ssh_known_hosts',
-      owner  => root,
-      group  => root,
-      mode   => '0644';
+  @@sshkey {
+    $::hostname:
+      host_aliases => [ $::fqdn, $::ipaddress_preferred ],
+      type         => 'ecdsa-sha2-nistp256',
+      key          => $::sshecdsakey;
   }
+
+  Sshkey <<| |>>
 }
