f793d2518f1a116fc40dfcf6f9eb50ceb2fc9b3e
Graeme Mathieson <mathie@woss.name>
2012-08-28 16:09:49 +0100

    Fix up the unescaped escape.

--- modules/postgresql/manifests/server/user.pp
+++ modules/postgresql/manifests/server/user.pp
@@ -2,7 +2,7 @@ define postgresql::server::user($password) {
   exec {
     "postgresql-user-${name}":
       command => "/usr/bin/psql -c \"CREATE ROLE ${name} UNENCRYPTED PASSWORD '${password}' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;\"",
-      unless  => "/usr/bin/psql -c '\du'| /bin/grep -q ${name}",
+      unless  => "/usr/bin/psql -c '\\du'| /bin/grep -q ${name}",
       user    => 'postgres',
       require => Class['postgresql::server'];
   }
