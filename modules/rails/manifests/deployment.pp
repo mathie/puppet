41ceac74d183e0b0429444681da5912d79f36879
Graeme Mathieson <mathie@woss.name>
2012-08-28 11:28:55 +0100

    Pass on the precompile_assets flag.

--- modules/rails/manifests/deployment.pp
+++ modules/rails/manifests/deployment.pp
@@ -3,14 +3,15 @@ define rails::deployment(
   $git_repo,
   $ssh_private_key,
   $ssh_authorized_keys,
-  $rails_env   = 'production',
-  $git_branch  = 'master',
-  $comment     = "${name} app server",
-  $db_type     = 'mysql2',
-  $database    = $name,
-  $db_host     = 'localhost',
-  $db_username = $name,
-  $db_password = ''
+  $rails_env         = 'production',
+  $git_branch        = 'master',
+  $comment           = "${name} app server",
+  $db_type           = 'mysql2',
+  $database          = $name,
+  $db_host           = 'localhost',
+  $db_username       = $name,
+  $db_password       = '',
+  $precompile_assets = true
 ) {
   $app_name = $name
 
@@ -24,14 +25,15 @@ define rails::deployment(
 
   rails::deployment::capistrano {
     $app_name:
-      git_repo    => $git_repo,
-      git_branch  => $git_branch,
-      rails_env   => $rails_env,
-      db_type     => $db_type,
-      database    => $database,
-      db_host     => $db_host,
-      db_username => $db_username,
-      db_password => $db_password;
+      git_repo          => $git_repo,
+      git_branch        => $git_branch,
+      rails_env         => $rails_env,
+      db_type           => $db_type,
+      database          => $database,
+      db_host           => $db_host,
+      db_username       => $db_username,
+      db_password       => $db_password,
+      precompile_assets => $precompile_assets;
   }
 
   Rails::Deployment::User[$app_name] -> Rails::Deployment::Capistrano[$app_name]
