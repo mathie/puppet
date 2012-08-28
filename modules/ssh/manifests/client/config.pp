0069f6b7479835cafd7deaf26bb07097cb4c0ea6
Graeme Mathieson <mathie@woss.name>
2012-08-28 11:17:09 +0100

    Turns out if puppet creates ssh known hosts, it makes it 0600 by default.

--- modules/ssh/manifests/client/config.pp
+++ modules/ssh/manifests/client/config.pp
@@ -1,4 +1,12 @@
 class ssh::client::config {
+  file {
+    '/etc/ssh/ssh_known_hosts':
+      ensure => present,
+      owner  => root,
+      group  => root,
+      mode   => '0644';
+  }
+
   @@sshkey {
     $::hostname:
       host_aliases => [ $::fqdn, $::ipaddress_preferred ],
