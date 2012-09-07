ae07504c1f2325fe1f6a27c141674d2196d5f8cc
Graeme Mathieson <mathie@woss.name>
2012-09-07 11:06:56 +0100

    Oops, remove dependency too.

--- modules/ntp/manifests/server.pp
+++ modules/ntp/manifests/server.pp
@@ -2,5 +2,4 @@ class ntp::server {
   include ntp::server::install, ntp::server::config, ntp::server::service
 
   Class['ntp::server::install'] -> Class['ntp::server::config'] ~> Class['ntp::server::service']
-  Class['apparmor::disable'] -> Class['ntp::server::service']
 }
