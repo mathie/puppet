6502931f68795787a68cc5e753b3974ed7b430e4
Graeme Mathieson <mathie@woss.name>
2012-09-07 10:53:42 +0100

    Gem packages can be explicitly versioned.
    Why doesn't it inherit this from the parent type?

--- modules/ruby/lib/puppet/provider/package/gem18.rb
+++ modules/ruby/lib/puppet/provider/package/gem18.rb
@@ -1,3 +1,5 @@
 Puppet::Type.type(:package).provide :gem18, :parent => :gem, :source => :gem do
+  has_feature :versionable
+
   commands :gemcmd => 'gem1.8'
 end
--- modules/ruby/lib/puppet/provider/package/gem19.rb
+++ modules/ruby/lib/puppet/provider/package/gem19.rb
@@ -1,3 +1,5 @@
 Puppet::Type.type(:package).provide :gem19, :parent => :gem, :source => :gem do
+  has_feature :versionable
+
   commands :gemcmd => 'gem1.9.1'
 end
