e7a3363c3cc7bf3c8c0efff11333961336075a80
Graeme Mathieson <mathie@woss.name>
2012-10-09 17:36:32 +0100

    Work around erlexec insisting that dollar_home is set.

--- modules/rabbitmq/manifests/server/plugin.pp
+++ modules/rabbitmq/manifests/server/plugin.pp
@@ -2,16 +2,18 @@ define rabbitmq::server::plugin($ensure = 'present') {
   if($ensure == 'present') {
     exec {
       "rabbitmq-enable-plugin-${name}":
-        command => "/usr/lib/rabbitmq/bin/rabbitmq-plugins enable ${name}",
-        unless  => "/usr/lib/rabbitmq/bin/rabbitmq-plugins list ${name} | /bin/grep '^.E. ${name} '",
-        notify  => Class['rabbitmq::server::service'];
+        command     => "/usr/lib/rabbitmq/bin/rabbitmq-plugins enable ${name}",
+        unless      => "/usr/lib/rabbitmq/bin/rabbitmq-plugins list ${name} | /bin/grep '^.E. ${name} '",
+        environment => "HOME=/root",
+        notify      => Class['rabbitmq::server::service'];
     }
   } else {
     exec {
       "rabbitmq-disable-plugin-${name}":
-        command => "/usr/lib/rabbitmq/bin/rabbitmq-plugins disable ${name}",
-        unless  => "/usr/lib/rabbitmq/bin/rabbitmq-plugins list ${name} | /bin/grep '^. . ${name} '",
-        notify  => Class['rabbitmq::server::service'];
+        command     => "/usr/lib/rabbitmq/bin/rabbitmq-plugins disable ${name}",
+        unless      => "/usr/lib/rabbitmq/bin/rabbitmq-plugins list ${name} | /bin/grep '^. . ${name} '",
+        environment => "HOME=/root",
+        notify      => Class['rabbitmq::server::service'];
     }
   }
 }
--- modules/rabbitmq/manifests/stomp.pp
+++ modules/rabbitmq/manifests/stomp.pp
@@ -1,9 +1,11 @@
 class rabbitmq::stomp {
   include rabbitmq::server
 
-  package {
-    'rabbitmq-stomp':
-      ensure => present,
+  exec {
+    'install-rabbitmq-stomp':
+      command     => '/usr/bin/apt-get -q -y -o DPkg::Options::=--force-confold install rabbitmq-stomp',
+      unless      => '/usr/bin/dpkg -l rabbitmq-stomp | /bin/grep ^ii',
+      environment => 'HOME=/root';
   }
 
   firewall::allow {
