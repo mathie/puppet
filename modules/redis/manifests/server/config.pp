f6fc618c446f594a233ad61c2120e89884aeeb21
Graeme Mathieson <mathie@woss.name>
2012-10-09 14:41:57 +0100

    Make redis available to places other that localhost.

--- modules/redis/manifests/server/config.pp
+++ modules/redis/manifests/server/config.pp
@@ -1,2 +1,10 @@
 class redis::server::config {
+  file {
+    '/etc/redis/redis.conf':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => template('redis/redis.conf.erb');
+  }
 }
