be779660fbf2e4666ccadf3c296bb0defe44cfbd
Graeme Mathieson <mathie@woss.name>
2012-10-09 14:40:58 +0100

    Make mco service and mco package actually work. Yay.

--- modules/mcollective/manifests/agent/config.pp
+++ modules/mcollective/manifests/agent/config.pp
@@ -5,6 +5,27 @@ class mcollective::agent::config {
       content => template('mcollective/server.cfg.erb'),
       owner   => root,
       group   => root,
-      mode    => '0600',
+      mode    => '0600';
+
+    # According to:
+    #
+    #  https://bugs.launchpad.net/ubuntu/+source/mcollective-plugins/+bug/1061287
+    #
+    # the reason that mcollective's service and package plugins haven't been
+    # working so far as becuase they're misnamed. Yay, I've been missing having
+    # access to mco service.
+    '/usr/share/mcollective/plugins/mcollective/agent/service.rb':
+      ensure => link,
+      target => '/usr/share/mcollective/plugins/mcollective/agent/puppet-service.rb',
+      owner  => root,
+      group  => root,
+      mode   => '0644';
+
+    '/usr/share/mcollective/plugins/mcollective/agent/package.rb':
+      ensure => link,
+      target => '/usr/share/mcollective/plugins/mcollective/agent/puppet-package.rb',
+      owner  => root,
+      group  => root,
+      mode   => '0644';
   }
 }
