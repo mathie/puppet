2da1816a0aada9f8264e5e4a30427329c725ed3c
Graeme Mathieson <mathie@woss.name>
2012-10-11 09:20:31 +0100

    Don't allow root login, or password-based login, from SSH.

--- modules/ssh/manifests/server/config.pp
+++ modules/ssh/manifests/server/config.pp
@@ -1,2 +1,10 @@
 class ssh::server::config {
+  file {
+    '/etc/ssh/sshd_config':
+      ensure => present,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => 'puppet:///modules/ssh/sshd_config';
+  }
 }
