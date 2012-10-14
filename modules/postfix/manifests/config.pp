d8a130d7c4071a6a6f7142cc5954f8dda133988f
Graeme Mathieson <mathie@woss.name>
2012-10-14 10:50:15 +0100

    Supply a basic master.cf with the cruft removed.

--- modules/postfix/manifests/config.pp
+++ modules/postfix/manifests/config.pp
@@ -1,5 +1,12 @@
 class postfix::config {
   file {
+    '/etc/postfix/master.cf':
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => template('postfix/master.cf.erb');
+
     '/etc/postfix/main.cf':
       ensure  => present,
       owner   => root,
