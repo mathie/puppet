cf4d70c2249bc81f42062090b707b4fc192d764c
Graeme Mathieson <mathie@woss.name>
2012-11-09 14:19:57 +0000

    Configure unattended upgrades.

--- modules/apt/manifests/unattended_upgrades/config.pp
+++ modules/apt/manifests/unattended_upgrades/config.pp
@@ -1,2 +1,10 @@
 class apt::unattended_upgrades::config {
+  file {
+    '/etc/apt/apt.conf.d/50unattended-upgrades':
+      ensure => present,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => 'puppet:///modules/apt/50unattended-upgrades';
+  }
 }
