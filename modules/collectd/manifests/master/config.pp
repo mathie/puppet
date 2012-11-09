53d19e8c689a3cb346422a615575d81b08cf9de4
Graeme Mathieson <mathie@woss.name>
2012-11-09 12:29:18 +0000

    Encrypt collectd communication and set appropriate max packet size.

--- modules/collectd/manifests/master/config.pp
+++ modules/collectd/manifests/master/config.pp
@@ -2,5 +2,14 @@ class collectd::master::config {
   include collectd::config
   include collectd::plugins::master
 
+  file {
+    '/etc/collectd/users':
+      ensure => present,
+      owner  => root,
+      group  => root,
+      mode   => '0600',
+      source => 'puppet:///modules/collectd/users';
+  }
+
   Class['collectd::config'] -> Class['collectd::plugins::master'] ~> Class['collectd::service']
 }
