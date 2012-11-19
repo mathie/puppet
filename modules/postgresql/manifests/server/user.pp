e1323132f482f35711a838166b7ab650c73a8087
Graeme Mathieson <mathie@woss.name>
2012-11-19 10:40:21 +0000

    Try again to get the order right.
    Don't try and create users before psql is installed...

--- modules/postgresql/manifests/server/user.pp
+++ modules/postgresql/manifests/server/user.pp
@@ -1,9 +1,13 @@
 define postgresql::server::user($password) {
+  include postgresql::client, postgresql::server
+
   exec {
     "postgresql-user-${name}":
       command => "/usr/bin/psql -c \"CREATE ROLE ${name} UNENCRYPTED PASSWORD '${password}' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;\"",
       unless  => "/usr/bin/psql -c '\\du'| /bin/grep -q ${name}",
-      user    => 'postgres',
-      require => [ Class['postgresql::server'], Class['postgresql::client'] ];
+      user    => 'postgres';
   }
+
+  Class['postgresql::client'] -> Exec["postgresql-user-${name}"]
+  Class['postgresql::server'] -> Exec["postgresql-user-${name}"]
 }
