c4f45647bac0dd68da435a82792d2cde15cf9dfd
Graeme Mathieson <mathie@woss.name>
2012-11-21 15:35:25 +0000

    Make sure template0 is unicode.

--- modules/postgresql/manifests/server/config.pp
+++ modules/postgresql/manifests/server/config.pp
@@ -1,4 +1,18 @@
 class postgresql::server::config {
+  exec {
+    'postgresql-drop-cluster':
+      command => '/usr/bin/pg_dropcluster --stop 9.1 main',
+      onlyif  => "/usr/bin/psql -c '\\l+' |awk '/template0/ { print \$5 }' |grep 'SQL_ASCII'",
+      user    => postgres,
+      notify  => Exec['postgresql-create-cluster'];
+
+    'postgresql-create-cluster':
+      command     => '/usr/bin/pg_createcluster --locale=en_GB.UTF-8 9.1 main',
+      user        => postgres,
+      refreshonly => true,
+      before      => [ File['/etc/postgresql/9.1/main/postgresql.conf'], File['/etc/postgresql/9.1/main/pg_hba.conf'] ];
+  }
+
   file {
     '/etc/postgresql/9.1/main/postgresql.conf':
       ensure  => present,
