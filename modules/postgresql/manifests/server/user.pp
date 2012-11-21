c1a1ea4f605c4e82086bd741b4c2352ffd02b0e3
Graeme Mathieson <mathie@woss.name>
2012-11-21 11:57:47 +0000

    Allow psql users to be superusers or not.

--- modules/postgresql/manifests/server/user.pp
+++ modules/postgresql/manifests/server/user.pp
@@ -1,9 +1,19 @@
-define postgresql::server::user($password) {
+define postgresql::server::user($password = undef, $superuser = false) {
   include postgresql::client, postgresql::server
+
+  $password_string = $password ? {
+    undef   => '',
+    default => "UNENCRYPTED PASSWORD '${password}'",
+  }
+
+  $permissions_string = $superuser ? {
+    true    => 'SUPERUSER CREATEDB CREATEROLE',
+    default => 'NOSUPERUSER NOCREATEDB NOCREATEROLE',
+  }
 
   exec {
     "postgresql-user-${name}":
-      command => "/usr/bin/psql -c \"CREATE ROLE ${name} UNENCRYPTED PASSWORD '${password}' NOSUPERUSER NOCREATEDB NOCREATEROLE INHERIT LOGIN;\"",
+      command => "/usr/bin/psql -c \"CREATE ROLE ${name} ${password_string} ${permissions_string} INHERIT LOGIN;\"",
       unless  => "/usr/bin/psql -c '\\du'| /bin/grep -q ${name}",
       user    => 'postgres';
   }
