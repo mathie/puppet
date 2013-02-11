1cb7c5eb4ca7c322653e5022531c68d71250d1b8
Graeme Mathieson <mathie@woss.name>
2013-02-11 14:30:01 +0000

    Anchor for the puppet dashboard class.

--- modules/puppet/manifests/dashboard.pp
+++ modules/puppet/manifests/dashboard.pp
@@ -3,5 +3,9 @@ class puppet::dashboard {
 
   include puppet::dashboard::install, puppet::dashboard::config, puppet::dashboard::service
 
-  Class['puppet::dashboard::install'] -> Class['puppet::dashboard::config'] ~> Class['puppet::dashboard::service']
+  anchor { 'puppet::dashboard::begin': } ->
+    Class['puppet::dashboard::install'] ->
+    Class['puppet::dashboard::config'] ~>
+    Class['puppet::dashboard::service'] ->
+    anchor { 'puppet::dashboard::end': }
 }
