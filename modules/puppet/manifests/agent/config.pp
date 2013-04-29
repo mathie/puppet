5ce1690a947d47cbc2f9d63a2ea673878cff9660
Graeme Mathieson <mathie@woss.name>
2013-04-29 14:40:20 +0100

    Skip pointless file content.

--- modules/puppet/manifests/agent/config.pp
+++ modules/puppet/manifests/agent/config.pp
@@ -2,11 +2,11 @@ class puppet::agent::config {
   include puppet::config
 
   file { '/etc/default/puppet':
-    ensure => present,
-    source => 'puppet:///modules/puppet/etc-default-puppet',
-    owner  => puppet,
-    group  => puppet,
-    mode   => '0644',
+    ensure  => present,
+    content => "START=yes\n",
+    owner   => puppet,
+    group   => puppet,
+    mode    => '0644',
   }
 
   anchor { 'puppet::agent::config::begin': } ->
