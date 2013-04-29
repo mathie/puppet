a1d727a2e6b4c5cd16a1e6282e764265598b95ae
Graeme Mathieson <mathie@woss.name>
2013-04-29 16:15:28 +0100

    Get the distribution right if there's no components.

--- modules/jenkins/manifests/repo.pp
+++ modules/jenkins/manifests/repo.pp
@@ -3,7 +3,7 @@ class jenkins::repo {
     'jenkins':
       url          => 'http://pkg.jenkins-ci.org/debian',
       keyid        => 'D50582E6',
-      distribution => '',
-      components   => [ 'binary' ];
+      distribution => 'binary',
+      components   => [ ];
   }
 }
