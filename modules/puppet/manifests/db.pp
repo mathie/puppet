9922216a599b3703ad68efbf2966d33c6cd127a8
Graeme Mathieson <mathie@woss.name>
2013-04-29 14:38:19 +0100

    Anchorify puppetdb too.

--- modules/puppet/manifests/db.pp
+++ modules/puppet/manifests/db.pp
@@ -1,9 +1,15 @@
 class puppet::db {
   include puppet::db::install, puppet::db::config, puppet::db::service
-  Class['puppet::db::install'] -> Class['puppet::db::config'] ~> Class['puppet::db::service']
+
+  anchor { 'puppet::db::begin': } ->
+    Class['puppet::db::install'] ->
+    Class['puppet::db::config'] ~>
+    Class['puppet::db::service'] ->
+    anchor { 'puppet::db::end': }
 
   # The puppet agent must have been configured and figured out its SSL
   # certificate before the DB can install itself. Otherwise it all goes
   # horribly wrong.
-  Class['puppet::master::service'] -> Class['puppet::agent::service'] -> Class['puppet::db::install']
+  Class['puppet::master'] -> Class['puppet::db']
+  Class['puppet::agent']  -> Class['puppet::db']
 }
