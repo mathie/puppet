a7a629ea20c9453f40b8be8bb8511f02f7fe9bc3
Graeme Mathieson <mathie@woss.name>
2013-04-29 11:33:07 +0100

    Provide a memcached config file.

--- modules/memcached/manifests/server/config.pp
+++ modules/memcached/manifests/server/config.pp
@@ -1,3 +1,10 @@
 class memcached::server::config {
-
+  file {
+    '/etc/memcached.conf':
+      ensure => present,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => 'puppet:///modules/memcached/memcached.conf';
+  }
 }
