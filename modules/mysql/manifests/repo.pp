9011b2934d1f5badf4a904785be865e0897e8f20
Graeme Mathieson <mathie@woss.name>
2013-04-30 10:17:05 +0100

    Prefer MySQL packages from the Percona repository.
    Since Percona is currently based on 5.5.30 and Ubuntu
    is distributing 5.5.31...

--- modules/mysql/manifests/repo.pp
+++ modules/mysql/manifests/repo.pp
@@ -4,4 +4,13 @@ class mysql::repo {
       url   => 'http://repo.percona.com/apt',
       keyid => 'CD2EFD2A';
   }
+
+  file {
+    '/etc/apt/preferences.d/pin-percona':
+      ensure => present,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => 'puppet:///modules/mysql/apt-preferences-pin-percona';
+  }
 }
