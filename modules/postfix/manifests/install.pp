04920983b9b40f3d4ef2693cdcc856d4b75a8e07
Graeme Mathieson <mathie@woss.name>
2013-04-30 10:11:08 +0100

    No need for a mailx command that needs mysql, right?

--- modules/postfix/manifests/install.pp
+++ modules/postfix/manifests/install.pp
@@ -3,7 +3,10 @@ class postfix::install {
     'postfix':
       ensure => present;
 
-    'mailutils':
+    # For a command line mail client. mailutils would seem like a sensible
+    # option, but it depends on libmysql, which gets a little hairy when we're
+    # trying to get percona client installed instead...
+    'bsd-mailx':
       ensure => present;
   }
 }
