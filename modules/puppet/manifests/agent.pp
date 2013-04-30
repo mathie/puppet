85c42ed2deab5fa126b1a8b0aa3541df3d3d68bc
Graeme Mathieson <mathie@woss.name>
2013-04-30 10:39:38 +0100

    Don't restart puppet on config changes.
    And leave a comment as to why, so I don't 'fix' it again.

--- modules/puppet/manifests/agent.pp
+++ modules/puppet/manifests/agent.pp
@@ -1,9 +1,15 @@
 class puppet::agent($puppetmaster_hostname = "puppet.${::domain}") {
   include puppet::agent::install, puppet::agent::config, puppet::agent::service
 
+  # The config -> service dependency is deliberately a require (->), not a
+  # notifiy (~>) for two reasons:
+  #
+  # * Puppet seems to get upset and stop working when it restarts itself.
+  #
+  # * Puppet automatically re-reads config files when they change anyway.
   anchor { 'puppet::agent::begin': } ->
     Class['puppet::agent::install'] ->
-    Class['puppet::agent::config'] ~>
+    Class['puppet::agent::config'] ->
     Class['puppet::agent::service'] ->
     anchor { 'puppet::agent::end': }
 }
--- modules/puppet/manifests/master.pp
+++ modules/puppet/manifests/master.pp
@@ -2,9 +2,15 @@ class puppet::master($ssh_key, $git_repo) {
   include puppet::agent
   include puppet::master::install, puppet::master::config, puppet::master::service
 
+  # The config -> service dependency is deliberately a require (->), not a
+  # notifiy (~>) for two reasons:
+  #
+  # * Puppet seems to get upset and stop working when it restarts itself.
+  #
+  # * Puppet automatically re-reads config files when they change anyway.
   anchor { 'puppet::master::begin': } ->
     Class['puppet::master::install'] ->
-    Class['puppet::master::config'] ~>
+    Class['puppet::master::config'] ->
     Class['puppet::master::service'] ->
     anchor { 'puppet::master::end': }
 }
