435419aaa5b1eb8ef903365a2525cc92df71c9e2
Graeme Mathieson <mathie@woss.name>
2013-05-03 12:35:55 +0100

    MySQL dev needs ssl-dev, too, really.

--- modules/mysql/manifests/dev.pp
+++ modules/mysql/manifests/dev.pp
@@ -1,9 +1,13 @@
 class mysql::dev {
   include mysql::repo
+  include ssl::dev
 
   package {
     'libmysqlclient-dev':
       ensure => present;
   }
 
+  anchor { 'mysql::dev::begin': } ->
+    Class['ssl::dev'] ->
+    anchor { 'mysql::dev::end': }
 }
