2ac644c12891dccb6d047c2c41dfae63a38f6d4f
Graeme Mathieson <mathie@woss.name>
2013-05-03 12:38:22 +0100

    Tweak dependencies.

--- modules/graphite/manifests/init.pp
+++ modules/graphite/manifests/init.pp
@@ -2,8 +2,15 @@ class graphite {
   # For now, the graphite module assumes it's running on the same host as the
   # collectd master because I'm lazy and don't feel like packaging up collectd
   # 5.1 which has a graphite output module.
-  include collectd::master, memcached::server
+  include collectd::master, memcached::server, gunicorn
   include graphite::install, graphite::config, graphite::service
 
-  Class['collectd::master'] -> Class['graphite::install'] -> Class['graphite::config'] ~> Class['graphite::service']
+  Class['collectd::master'] -> Class['graphite']
+  Class['gunicorn']         -> Class['graphite']
+
+  anchor { 'graphite::begin': } ->
+    Class['graphite::install'] ->
+    Class['graphite::config'] ~>
+    Class['graphite::service'] ->
+    anchor { 'graphite::end': }
 }
