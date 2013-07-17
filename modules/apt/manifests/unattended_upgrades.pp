af44b82a1a741c25fa3017fc22529d09dbe47214
Graeme Mathieson <mathie@woss.name>
2013-07-17 16:25:47 +0100

    Allow unattended upgrades email to be parameterised.

--- modules/apt/manifests/unattended_upgrades.pp
+++ modules/apt/manifests/unattended_upgrades.pp
@@ -1,4 +1,4 @@
-class apt::unattended_upgrades {
+class apt::unattended_upgrades($notify_email = undef) {
   include apt::unattended_upgrades::install, apt::unattended_upgrades::config
 
   Class['apt::unattended_upgrades::install'] ->
--- modules/apt/manifests/unattended_upgrades/config.pp
+++ modules/apt/manifests/unattended_upgrades/config.pp
@@ -1,4 +1,6 @@
 class apt::unattended_upgrades::config {
+  $notify_email = $apt::unattended_upgrades::notify_email
+
   file {
     '/etc/apt/apt.conf.d/10periodic':
       ensure => present,
@@ -8,10 +10,10 @@ class apt::unattended_upgrades::config {
       source => 'puppet:///modules/apt/10periodic';
 
     '/etc/apt/apt.conf.d/50unattended-upgrades':
-      ensure => present,
-      owner  => root,
-      group  => root,
-      mode   => '0644',
-      source => 'puppet:///modules/apt/50unattended-upgrades';
+      ensure  => present,
+      owner   => root,
+      group   => root,
+      mode    => '0644',
+      content => template('apt/50unattended-upgrades.erb');
   }
 }
