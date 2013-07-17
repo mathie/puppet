07926aac02350a7ab6516ccb6fa6885afaab4ee5
Graeme Mathieson <mathie@woss.name>
2013-07-17 16:44:12 +0100

    Pin to the stable nginx, not from ruby-ng.

--- modules/ruby/manifests/repo.pp
+++ modules/ruby/manifests/repo.pp
@@ -4,4 +4,13 @@ class ruby::repo($stage = first) {
       url   => 'http://ppa.launchpad.net/brightbox/ruby-ng-experimental/ubuntu',
       keyid => 'C3173AA6';
   }
+
+  file {
+    '/etc/apt/preferences.d/pin-ruby':
+      ensure => present,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => 'puppet:///modules/ruby/apt-preferences-pin-ruby';
+  }
 }
