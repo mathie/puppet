a89b7fb6eb864eb484d4efe9ac4cc77d6fdb7b88
Graeme Mathieson <mathie@woss.name>
2013-07-22 11:53:26 +0100

    Don't install recommended packages, just deps.

--- modules/apt/manifests/init.pp
+++ modules/apt/manifests/init.pp
@@ -9,4 +9,13 @@ class apt {
       command     => '/usr/bin/apt-get update',
       refreshonly => true;
   }
+
+  file {
+    '/etc/apt/apt.conf.d/50minimalism':
+      ensure => present,
+      owner  => root,
+      group  => root,
+      mode   => '0644',
+      source => 'puppet:///modules/apt/50minimalism';
+  }
 }
