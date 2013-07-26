e3b2cddc5b4dec2d6ecb7ee5d0212e901f50794b
Graeme Mathieson <mathie@woss.name>
2013-07-26 11:14:12 +0100

    Update Ewgeco standard class to have users and working unattended upgrades.

--- manifests/classes/ewgeco.pp
+++ manifests/classes/ewgeco.pp
@@ -11,5 +11,26 @@ class standard::ewgeco {
       relayhost   => 'smtp.sendgrid.net',
       username    => 'gordon@ewgeco.com',
       password    => 'telf0rd*7';
+
+    'apt::unattended_upgrades':
+      notify_email => 'mathie@rubaidh.com';
+  }
+
+  users::account {
+    'mathie':
+      uid      => 10001,
+      password => '$6$sIQnqvVz$LOocGXi65myfyIne7knOr0KL0QkjReLbuSe9Fe5ct.jGOVTWf6NID4toF6Pkm5I5nRldC4CtcC.kyLo6ddZKQ0',
+      htpasswd => '$apr1$z7n8mzjB$rNsr7NhLnqPDnR.Pd20zz0',
+      comment  => 'Graeme Mathieson',
+      email    => 'mathie@rubaidh.com',
+      sudo     => true;
+
+    'ewgeco':
+      uid                 => 10002,
+      comment             => 'Ewgeco Operations',
+      htpasswd            => '$apr1$hRNHk998$kuPFRkI/RyEUWcsRChX5m1', # xieZi8ie
+      ssh_authorized_keys => false,
+      email               => 'operations@ewgeco.com';
+
   }
 }
